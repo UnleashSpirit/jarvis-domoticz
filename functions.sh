edb004a5d5a36c20ec5917503cec3c3561ac9753
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2017-11-29 21:30:50 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -55,13 +55,9 @@ fi
 #Fonction de recuperation de l'idx, via les devices en favoris
 pg_dz_idx () {
 local pg_dz_device="$(curl -s "http://${pg_dz_domoticz_ip}:${pg_dz_domoticz_port}/json.htm?type=devices&used=true&filter=all&favorite=1")"
-	echo "Beta 1:" $order
 local -r order="$(jv_sanitize "$order")"
-	echo "Beta 2:" $order
     while read device; do
-	echo "Beta 3:"$device
         local sdevice="$(jv_sanitize "$device" ".*")"
-			echo "Beta 4:"$sdevice
 		if [[ "$order" =~ .*$sdevice.* ]]; then
             local idx="$(echo $pg_dz_device | jq -r ".result[] | select(.Name==\"$device\") | .idx")"
 			return $idx
