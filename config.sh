148151d3ccbde88678a19bb47d90d4210d158786
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2017-11-29 21:07:44 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,4 @@
-#Change for your own value
+#Change for your own value @IP of Domoticz on localhost
 
-pg_dz_domoticz_ip="192.168.0.38"
+pg_dz_domoticz_ip="localhost"
 pg_dz_domoticz_port="8080"
-
-
-#Don't touch
-pg_dz_api_switch="type=command&param=switchlight&switchcmd="
-pg_dz_api_temp="type=devices"
