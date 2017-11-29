881c203a5bc284db117dc873e6d1d5b7a8137e3d
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2017-11-29 21:04:38 +0100

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -2,13 +2,13 @@
 
 pg_dz_lg() {
     case "$1" in
-        no_device_matching) warning "Aucun appareil ne correspond. Ajoutez le dans vos favoris Domoticz, ou declarez un incident si il s'y trouve deja.";;
+        no_device_matching) jv_warning "Aucun appareil ne correspond. Ajoutez le dans vos favoris Domoticz, ou declarez un incident si il s'y trouve deja.";;
         switch_on) echo "J'ai allumé $2";;
         switch_off) echo "J'ai éteins $2";;
         blind_on) echo "J'ai fermé $2";;
         blind_off) echo "J'ai ouvert $2";;
 		stat) echo "Je regarde... $2:";;
 		temp) echo "La temperature $2 est de:";;
-        *) error "ERROR: ";;
+        *) jv_error "ERROR: ";;
     esac
 }
